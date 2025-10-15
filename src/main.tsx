750843ffeec5d4c81f08d82dea46c499b48b0c15
hanjiwon1108 <jwjwhan1108@gmail.com>
2025-10-15 14:54:53 +0900

    feat: chat navigate 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,27 +1,25 @@
-import { createRoot } from 'react-dom/client'
+import { createRoot } from "react-dom/client";
 
-import {
-    BrowserRouter,
-    Routes,
-    Route
-} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import { RecoilRoot } from 'recoil'
-import { GlobalStyles } from './styles/GlobalStyles'
-import LoginPage from './pages/login'
-import ProfilePage from './pages/profile'
-import CountdownPage from './pages/countdown'
+import { RecoilRoot } from "recoil";
+import { GlobalStyles } from "./styles/GlobalStyles";
+import LoginPage from "./pages/login";
+import ProfilePage from "./pages/profile";
+import CountdownPage from "./pages/countdown";
+import ChatPage from "./pages/chat";
 
-createRoot(document.getElementById('root')!).render(
-   <RecoilRoot>
+createRoot(document.getElementById("root")!).render(
+  <RecoilRoot>
     <GlobalStyles />
     <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<LoginPage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/profile" element={<ProfilePage />} />
-            <Route path="/countdown" element={<CountdownPage />} />
-        </Routes>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/countdown" element={<CountdownPage />} />
+        <Route path="/chat" element={<ChatPage />} />
+      </Routes>
     </BrowserRouter>
-</RecoilRoot>
-)
+  </RecoilRoot>
+);
