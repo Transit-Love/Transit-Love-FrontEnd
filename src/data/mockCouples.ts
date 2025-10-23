62cb26059dc167b4946b812b7779988c4e6bbac3
hanjiwon1108 <jwjwhan1108@gmail.com>
2025-10-24 08:22:28 +0900

    feat: 임시 mock 데이터 추가

---
+++ src/data/mockCouples.ts
@@ -0,0 +1,32 @@
+import type { Couple } from "../types/couple";
+
+export const mockCouples: Couple[] = [
+  {
+    id: 1,
+    leftName: "온니온니허온니",
+    rightName: "너도 아라를 아라?",
+    leftAvatar: "/src/assets/icon/avatar1.png",
+    rightAvatar: "/src/assets/icon/avatar2.png",
+  },
+  {
+    id: 2,
+    leftName: "사진작가 이효준",
+    rightName: "나도 연애하고 싶다",
+    leftAvatar: "/src/assets/icon/avatar3.png",
+    rightAvatar: "/src/assets/icon/avatar4.png",
+  },
+  {
+    id: 3,
+    leftName: "에빈니",
+    rightName: "승찬니",
+    leftAvatar: "/src/assets/icon/avatar2.png",
+    rightAvatar: "/src/assets/icon/avatar3.png",
+  },
+  {
+    id: 4,
+    leftName: "민주핑",
+    rightName: "효준핑",
+    leftAvatar: "/src/assets/icon/avatar4.png",
+    rightAvatar: "/src/assets/icon/avatar1.png",
+  },
+];
